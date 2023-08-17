62569ad40cc9775280438fcaad66e8587e672c15
hatahata in Nagahara lab <ratdotl.hata@gmail.com>
2023-08-17 19:09:36 +0900

    feat: ubuntu

--- .zshrc_Linux.zsh
+++ .zshrc_Linux.zsh
@@ -1,1 +1,21 @@
-echo "There is no settings for Linux"
+########################################
+# PATH
+#######################################
+[[ -d $HOME/bin ]] && export PATH=$PATH:$HOME/bin
+[[ -d /usr/local/cuda ]] && export PATH=$PATH:/usr/local/cuda/bin
+
+#######################################
+# some applecation
+#######################################
+
+# for asdf
+if [[ -f $HOME/.asdf/asdf.sh ]]; then
+    . "$HOME/.asdf/asdf.sh"
+    fpath=(${ASDF_DIR}/completions $fpath)
+    autoload -Uz compinit && compinit
+fi
+
+########################################
+# Key Mappings
+#######################################
+xmodmap ~/.Xmodmap
