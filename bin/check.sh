54bf9fd1fc40c7c93cfb6d2934c5e2e5b862585d
atahatah <ratdotl.hata@gmail.com>
2023-08-26 16:18:20 +0900

    feat check updates

--- bin/check.sh
+++ bin/check.sh
@@ -2,20 +2,21 @@
 
 DOTFILES_HOME="$HOME/dotfiles"
 
+autoload -Uz colors
+colors
+echo "${fg[cyan]}Checking the updates...${reset_color}" 
 if test -n "$(git -C $DOTFILES_HOME status --porcelain)"; then
-    autoload -Uz colors
-    colors
     echo ""
-    echo "${fg[cyan]}==================WORNING==================${reset_color}" 
-    echo "${fg[cyan]}there are ${fg[magenta]}LOCALLY ${fg[cyan]}some changes of dotfiles.${reset_color}" 
-    echo "${fg[cyan]}===========================================${reset_color}" 
+    echo "${fg[cyan]}=====================WORNING======================${reset_color}" 
+    echo "${fg[cyan]}There are uncommitted changes related to dotfiles.${reset_color}" 
+    echo "${fg[cyan]}==================================================${reset_color}" 
     echo ""
 elif ! git -C $DOTFILES_HOME diff --exit-code --stat --cached origin/main > /dev/null; then
-    autoload -Uz colors
-    colors
     echo ""
-    echo "${fg[cyan]}==================WORNING==================${reset_color}" 
-    echo "${fg[cyan]}thre are ${fg[magenta]}REMOTELY ${fg[cyan]}some changes of dotfiles.${reset_color}" 
-    echo "${fg[cyan]}===========================================${reset_color}" 
+    echo "${fg[cyan]}===================WORNING===================${reset_color}" 
+    echo "${fg[cyan]}It is not in sync with the remote repository.${reset_color}" 
+    echo "${fg[cyan]}=============================================${reset_color}" 
     echo ""
+else
+    echo "${fg[green]}Anything related to dotfiles is the latest!${reset_color}" 
 fi
