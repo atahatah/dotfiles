2c4e186373a12c3ff2a965a38981a870659322a9
atahatah <ratdotl.hata@gmail.com>
2023-12-21 10:02:53 +0900

    add mas to install line

--- bin/opt/Darwin/installer.sh
+++ bin/opt/Darwin/installer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 . $HOME/dotfiles/bin/opt/Darwin/brew/install_brew.sh
 . $HOME/dotfiles/bin/opt/Darwin/karabiner-elements/import-settings.sh
+. $HOME/dotfiles/bin/opt/Darwin/mas/mas.sh
---
+++ bin/opt/Darwin/mas/mas.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# LINE
+mas install 539883307
